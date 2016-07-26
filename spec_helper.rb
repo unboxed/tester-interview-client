6a50cd4a97932ead584a63cfe2e6cf0a2a41958b
Charlie Egan <git@charlieegan3.com>
2016-07-26 13:14:43 +0100

    Allow requests to remote hosts

--- spec_helper.rb
+++ spec_helper.rb
@@ -11,3 +11,7 @@ Capybara.configure do |config|
   config.default_driver = :webkit
   config.app_host = ENV["ACTIONS_SERVER_URL"]
 end
+
+Capybara::Webkit.configure do |config|
+  config.allow_url(ENV["ACTIONS_SERVER_URL"])
+end
