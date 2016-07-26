253e87ecd63542aeb06830e6b585e319b2f59445
Charlie Egan <git@charlieegan3.com>
2016-07-26 16:19:20 +0100

    Add task intro comment & test case for empty input

--- actions_spec.rb
+++ actions_spec.rb
@@ -1,5 +1,8 @@
 require "./spec_helper"
 
+# Tests for an 'action' on a server that computes values in the fibonacci
+# sequence (0 1 1 2 3 5 8 13 21...)
+
 RSpec.describe "fibonacci action" do
   include Capybara::DSL
 
@@ -13,5 +16,11 @@ RSpec.describe "fibonacci action" do
     visit("/actions/fibonacci/5")
     response = JSON.parse(page.body)
     expect(response["answer"]).to eq(5)
+  end
+
+  it "returns an error for blank input" do
+    visit("/actions/fibonacci/")
+    status = page.status_code
+    expect(status).to eq(400)
   end
 end
